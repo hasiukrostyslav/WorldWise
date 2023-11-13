95288c29987ea6c9c8bd5529e58026acbeb11449
Rostyslav <hasiukrostyslav@gmail.com>
2023-11-13 20:45:17 +0200

    added error page

--- src/pages/HomeErrorPage.tsx
+++ src/pages/HomeErrorPage.tsx
@@ -1,5 +1,21 @@
+import styled from 'styled-components';
+
+const StyledError = styled.div`
+  flex-grow: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  h2 {
+    font-size: 4rem;
+  }
+`;
+
 function HomeErrorPage() {
-  return <div>Error</div>;
+  return (
+    <StyledError>
+      <h2>Page not found.</h2>
+    </StyledError>
+  );
 }
 
 export default HomeErrorPage;
