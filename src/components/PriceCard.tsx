10839a01e25a3c572321cac6ad5ddfa5dc8a5e0a
Rostyslav <hasiukrostyslav@gmail.com>
2023-11-13 20:14:45 +0200

    added pricing page

---
+++ src/components/PriceCard.tsx
@@ -0,0 +1,43 @@
+import styled from 'styled-components';
+import Button from './Button';
+
+interface PriceCardProps {
+  plan: string;
+  price: string;
+  device: number;
+}
+
+const StyledPriceCard = styled.div`
+  background-color: var(--color-dark--1);
+  width: 35rem;
+  padding: 4rem 5rem;
+  border-radius: 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  h2 {
+    font-size: 4rem;
+    padding: 2rem 0;
+  }
+
+  span {
+    font-size: 1.4rem;
+    padding-bottom: 4rem;
+  }
+`;
+
+function PriceCard({ plan, price, device }: PriceCardProps) {
+  return (
+    <StyledPriceCard>
+      <h3>{plan}</h3>
+      <h2>{price}</h2>
+      <span>
+        {device} {device === 1 ? 'device' : 'devices'}
+      </span>
+      <Button to="/login">Get started</Button>
+    </StyledPriceCard>
+  );
+}
+
+export default PriceCard;
--- src/pages/Pricing.tsx
+++ src/pages/Pricing.tsx
@@ -1,5 +1,22 @@
+import styled from 'styled-components';
+import PriceCard from '../components/PriceCard';
+
+const StyledPricing = styled.section`
+  flex-grow: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 2rem;
+`;
+
 function Pricing() {
-  return <div>Price</div>;
+  return (
+    <StyledPricing>
+      <PriceCard plan="Start" price="Free" device={1} />
+      <PriceCard plan="Basic" price="$7.99" device={3} />
+      <PriceCard plan="Premium" price="$12.99" device={5} />
+    </StyledPricing>
+  );
 }
 
 export default Pricing;
