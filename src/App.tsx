920f5fd31d524c9e5642c57fcf4e5e0254ddd5f6
Rostyslav <hasiukrostyslav@gmail.com>
2023-11-20 11:56:05 +0200

    added React Query configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import GlobalStyles from './styles/GlobalStyles';
 import HomeLayout from './pages/HomeLayout';
@@ -16,9 +18,11 @@ import Country from './components/Country';
 import Error from './components/Error';
 import AddForm from './components/AddForm';
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
@@ -41,7 +45,8 @@ function App() {
           </Route>
         </Routes>
       </BrowserRouter>
-    </>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
