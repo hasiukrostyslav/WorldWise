ce8d7354f5aed2cc27f8594af86625efaf1de9b5
Rostyslav <hasiukrostyslav@gmail.com>
2023-11-21 10:29:30 +0200

    added leaflet map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,5 +1,35 @@
+import styled from 'styled-components';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+
+import User from './User';
+
+const StyledMap = styled.section`
+  position: relative;
+  flex-grow: 1;
+`;
+
 function Map() {
-  return <div>Map</div>;
+  return (
+    <StyledMap>
+      <User />
+      <MapContainer
+        style={{ height: '100%' }}
+        center={[51.505, -0.09]}
+        zoom={6}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </StyledMap>
+  );
 }
 
 export default Map;
