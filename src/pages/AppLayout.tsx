685580574aea889db592f2643dddbb5e49e48dbe
Rostyslav <hasiukrostyslav@gmail.com>
2024-01-13 11:04:57 +0200

    change layout style for small screens

--- src/pages/AppLayout.tsx
+++ src/pages/AppLayout.tsx
@@ -2,17 +2,23 @@ import { Suspense, lazy } from 'react';
 import { Outlet } from 'react-router-dom';
 import styled from 'styled-components';
 
+import { SCREEN_SIZE, mediaQueries } from '../styles/mediaQueries';
+
 import Sidebar from '../components/Sidebar';
 import MapSpinner from '../components/MapSpinner';
 
 const Map = lazy(() => import('../components/Map'));
 
 const StyledAppLayout = styled.section`
-  min-height: calc(100vh - 5rem);
-  margin: 2.5rem;
-  border-radius: 1rem;
+  min-height: 100vh;
   overflow: hidden;
   position: relative;
+
+  ${mediaQueries(SCREEN_SIZE.Tablet)` 
+    margin: 2.5rem;
+    border-radius: 1rem;
+    min-height: calc(100vh - 5rem);
+  `}
 `;
 
 function AppLayout() {
--- src/pages/HomeLayout.tsx
+++ src/pages/HomeLayout.tsx
@@ -1,13 +1,14 @@
 import { Outlet } from 'react-router-dom';
 import { styled } from 'styled-components';
 
+import { SCREEN_SIZE, mediaQueries } from '../styles/mediaQueries';
+
 import Navbar from '../components/Navbar';
 
 const Layout = styled.div`
   display: flex;
   flex-direction: column;
-  min-height: calc(100vh - 5rem);
-  margin: 2.5rem;
+  min-height: 100vh;
   padding: 2.5rem 5rem;
   background-image: linear-gradient(
       rgba(36, 42, 46, 0.8),
@@ -16,7 +17,12 @@ const Layout = styled.div`
     url('/bg.jpg');
   background-size: cover;
   background-position: center;
-  border-radius: 1rem;
+
+  ${mediaQueries(SCREEN_SIZE.Tablet)` 
+    margin: 2.5rem;
+    border-radius: 1rem;
+    min-height: calc(100vh - 5rem);
+  `}
 `;
 
 function HomeLayout() {
