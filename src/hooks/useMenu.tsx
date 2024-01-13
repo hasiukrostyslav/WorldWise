4529c8c2de85b1ed482cbff87e32537cb5468197
Rostyslav <hasiukrostyslav@gmail.com>
2024-01-13 15:11:53 +0200

    fixed menu context

--- src/hooks/useMenu.tsx
+++ src/hooks/useMenu.tsx
@@ -4,8 +4,7 @@ import { MenuContext } from '../context/MenuContext';
 export function useMenu() {
   const context = useContext(MenuContext);
 
-  if (!context)
-    throw new Error('useNavigateMenu has to be used within ScreenSizeContext');
+  if (!context) throw new Error('useMenu has to be used within MenuProvider');
 
   const isOpenMenu = context.isOpenMenu;
   const closeMenu = context.closeMenu;
